6c3e761d49a1d12c0b2fa4e79a0546937ac55959
rargelaguet <ricard.argelaguet@gmail.com>
2020-06-13 11:39:17 +0200

    update settings

--- settings.R
+++ settings.R
@@ -10,12 +10,11 @@ io <- list()
 if (grepl("ricard",Sys.info()['nodename'])) {
   io$basedir <- "/Users/ricard/data/10x_gastrulation_DNMTs"
   io$atlas.basedir <- "/Users/ricard/data/gastrulation10x"
-  # io$gene_metadata <- "/Users/ricard/data/ensembl/mouse/v87/BioMart/mRNA/Mmusculus_genes_BioMart.87.txt"
   io$gene_metadata <- "/Users/ricard/data/ensembl/mouse/v87/BioMart/all_genes/Mmusculus_genes_BioMart.87.txt"
 } else if (grepl("ebi",Sys.info()['nodename'])) {
   io$basedir <- "/hps/nobackup2/research/stegle/users/ricard/10x_gastrulation_DNMTs"
   io$atlas.basedir <- "/hps/nobackup2/research/stegle/users/ricard/gastrulation10x"
-  io$gene_metadata <- "/hps/nobackup2/research/stegle/users/ricard/ensembl/mouse/v87/BioMart/mRNA/Mmusculus_genes_BioMart.87.txt"
+  io$gene_metadata <- "/hps/nobackup2/research/stegle/users/ricard/ensembl/mouse/v87/BioMart/all_genes/Mmusculus_genes_BioMart.87.txt"
 } else {
   stop("Computer not recognised")
 }
@@ -26,7 +25,7 @@ io$sce <- paste0(io$basedir,"/processed/SingleCellExperiment.rds")
 
 # Atlas information
 io$atlas.metadata <- paste0(io$atlas.basedir,"/sample_metadata.txt.gz")
-io$atlas.marker_genes <- paste0(io$atlas.basedir,"/results/marker_genes/marker_genes.txt.gz")
+io$atlas.marker_genes <- paste0(io$atlas.basedir,"/results/marker_genes/all_stages/marker_genes.txt.gz")
 io$atlas.differential <- paste0(io$atlas.basedir,"/results/differential")
 io$atlas.average_expression_per_celltype <- paste0(io$atlas.basedir,"/results/marker_genes/avg_expr_per_celltype_and_gene.txt.gz")
 io$atlas.sce <- paste0(io$atlas.basedir,"/processed/SingleCellExperiment.rds")
@@ -119,23 +118,48 @@ opts$celltype.colors = c(
 )
 
 opts$batches <- c(
-  "E125_DNMT3A_HET_A_L001", 
-  "E125_DNMT3A_HET_A_L003", 
-  "E125_DNMT3A_KO_B_L002", 
-  "E125_DNMT3A_KO_E_L004", 
   "SIGAA6_E85_2_Dnmt3aKO_Dnmt3b_WT_L001", 
   "SIGAB6_E85_3_Dnmt3aWT_Dnmt3b_WT_L002", 
   "SIGAC6_E85_5_Dnmt3aKO_Dnmt3b_Het_L003", 
-  "SIGAD6_E85_8_Dnmt3aHet_Dnmt3b_KO_L004"
+  "SIGAD6_E85_8_Dnmt3aHet_Dnmt3b_KO_L004",
+  "15_E8_5_D3A_WT_D3B_WT_L007",
+  "17_E8_5_D3A_KO_D3B_WT_L008",
+  "2_E8_5_D3A_WT_D3B_KO_L003",
+  "3_E8_5_D3A_HET_D3B_WT_L004",
+  "7_E8_5_D3A_WT_D3B_KO_L005",
+  "8_E8_5_D3A_KO_D3B_KO_L006"
+  
+  # "E125_DNMT3A_HET_A_L001", 
+  # "E125_DNMT3A_HET_A_L003", 
+  # "E125_DNMT3A_KO_B_L002", 
+  # "E125_DNMT3A_KO_E_L004", 
+  # "A_E12_5_D3a_Het_L001",
+  # "B_E12_5_D3a_KO_L002"
+)
+
+opts$stages <- c(
+  # "E12.5",
+  "E8.5"
 )
 
 opts$classes <- c(
-  "E8.5_Dnmt3aKO_Dnmt3bWT", 
-  "E8.5_Dnmt3aWT_Dnmt3bWT", 
-  "E8.5_Dnmt3aKO_Dnmt3bHET", 
+  # "E12.5_Dnmt3aWT_Dnmt3bHET",
+  # "E12.5_Dnmt3aWT_Dnmt3bKO",
+  # "E125_Dnmt3aHET_Dnmt3bWT",
+  # "E125_Dnmt3aKO_Dnmt3bWT",
+  # "E125_Dnmt3aWT_Dnmt3bHET",
+  # "E125_Dnmt3aWT_Dnmt3bKO",
   "E8.5_Dnmt3aHET_Dnmt3bKO",
-  "E12.5_Dnmt3aWT_Dnmt3bHET", 
-  "E12.5_Dnmt3aWT_Dnmt3bKO" 
+  "E8.5_Dnmt3aKO_Dnmt3bHET",
+  "E8.5_Dnmt3aKO_Dnmt3bWT",
+  "E8.5_Dnmt3aWT_Dnmt3bWT",
+  "E85_Dnmt3aHet_Dnmt3bKO",
+  "E85_Dnmt3aHET_Dnmt3bWT",
+  "E85_Dnmt3aKO_Dnmt3bHet",
+  "E85_Dnmt3aKO_Dnmt3bKO",
+  "E85_Dnmt3aKO_Dnmt3bWT",
+  "E85_Dnmt3aWT_Dnmt3bKO",
+  "E85_Dnmt3aWT_Dnmt3bWT"
 )
 
 ##########################
